1db5587bd3f6d589d3d5f352ee46b6ae98bd18d5
turingmachine42 <twunderling91@gmail.com>
2014-04-28 12:06:30 +0200

    rudimentary background subtraction implemented; parameters still need fine tuning

--- MyNewOpenCVTestProject/MyNewOpenCVTestProject/main.cpp
+++ MyNewOpenCVTestProject/MyNewOpenCVTestProject/main.cpp
@@ -1,36 +1,53 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/background_segm.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
+#include <vector>
 #include <iostream>
-
-using namespace cv;
-using namespace std;
 
 int main(int argc, char** argv)
 {
-
-	VideoCapture sequence("C:/Users/Tom/Pictures/training/6/0000000000.png");
+	// Input Stream from Source
+	cv::VideoCapture sequence("C:/training/2/0000000000.png");
 	if (!sequence.isOpened())
 	{
-		cerr << "Failed to open Image Sequence!\n" << endl;
+		std::cerr << "Failed to open Image Sequence!\n" << std::endl;
 		return 1;
 	}
 
-	Mat image;
-	namedWindow("Image | q or esc to quit", CV_WINDOW_NORMAL);
+	cv::Mat frame;	// current frame
+	cv::Mat back;	// background image
+	cv::Mat fore;	// foreground mask
+
+	cv::BackgroundSubtractorMOG2 mog(0, 16, false);
+	std::vector<std::vector<cv::Point> > contours;
+
+	//cv::namedWindow("Image | q or esc to quit", CV_WINDOW_NORMAL);
 
 	for (;;)
 	{
-		sequence >> image;
-		if (image.empty())
+		sequence >> frame;
+		if (frame.empty())
 		{
-			cout << "End of Sequence" << endl;
+			std::cout << "End of Sequence" << std::endl;
 			break;
 		}
 
-		imshow("image | q or esc to quit", image);
+		mog.operator()(frame, fore);
+		mog.getBackgroundImage(back);
 
-		char key = (char)waitKey(50);
+		cv::erode(fore, fore, cv::Mat(), cv::Point(-1, -1), 3);
+		cv::dilate(fore, fore, cv::Mat(), cv::Point(-1, -1), 3);
+
+		cv::findContours(fore, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+		cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0), 1);
+
+		
+		imshow("input | q or esc to quit", frame);
+		imshow("background | q or esc to quit", back);
+
+		char key = (char)cv::waitKey(1);
 		if (key == 'q' || key == 'Q' || key == 27)
 			break;
 	}

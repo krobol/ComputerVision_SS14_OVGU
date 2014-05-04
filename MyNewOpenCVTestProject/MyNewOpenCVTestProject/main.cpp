724e5bc9254b425047e5612d44fd55c9d7bd5f73
dita <dita@hotmail.de>
2014-05-04 19:45:36 +0200

    pausieren und wechseln zum nächsten/letzten Bild mit + und - eingebaut Strukturelemente für erosion und dilation angegeben Kanten werden iteriert und zu kurze Kanten rausgeworfen

--- MyNewOpenCVTestProject/MyNewOpenCVTestProject/main.cpp
+++ MyNewOpenCVTestProject/MyNewOpenCVTestProject/main.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char** argv)
 {
 	// Input Stream from Source
-	cv::VideoCapture sequence("C:/training/2/0000000000.png");
+	cv::VideoCapture sequence("D:/Download/training/2/0000000000.png");
 	if (!sequence.isOpened())
 	{
 		std::cerr << "Failed to open Image Sequence!\n" << std::endl;
@@ -19,17 +19,21 @@ int main(int argc, char** argv)
 	cv::Mat frame;	// current frame
 	cv::Mat back;	// background image
 	cv::Mat fore;	// foreground mask
-
-	cv::BackgroundSubtractorMOG2 mog(0, 16, false);
+	cv::Mat structure = cv::Mat::ones(5, 5, CV_8UC1); // strukturelement für dilation
+	bool pause = false;
+	bool repause = false;
+	cv::BackgroundSubtractorMOG2 mog(50, 3, false);
 	std::vector<std::vector<cv::Point> > contours;
-
+	//std::cout << "O = " << std::endl << " " << structure << std::endl << std::endl;
 	//cv::namedWindow("Image | q or esc to quit", CV_WINDOW_NORMAL);
 
-	for (;;)
+	while (true)
 	{
-		sequence >> frame;
+		if(pause == false){
+					sequence >> frame;
 		if (frame.empty())
 		{
+			
 			std::cout << "End of Sequence" << std::endl;
 			break;
 		}
@@ -37,20 +41,50 @@ int main(int argc, char** argv)
 		mog.operator()(frame, fore);
 		mog.getBackgroundImage(back);
 
-		cv::erode(fore, fore, cv::Mat(), cv::Point(-1, -1), 3);
-		cv::dilate(fore, fore, cv::Mat(), cv::Point(-1, -1), 3);
+		cv::erode(fore, fore, cv::Mat::ones(2, 2, CV_8UC1), cv::Point(-1, -1), 1);
+		cv::dilate(fore, fore, structure, cv::Point(-1, -1), 1);
+
+	
 
 		cv::findContours(fore, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+		for(int i = contours.size()-1; i > -1; i--){
+			if(contours[i].size() < 150)
+				contours.erase(contours.begin()+i);
+		}
 		cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0), 1);
 
 		
 		imshow("input | q or esc to quit", frame);
-		imshow("background | q or esc to quit", back);
+		//imshow("background | q or esc to quit", fore);
+
+		if (repause){
+			repause = false;
+			pause = true;
+		}
+
+		}
+
 
 		char key = (char)cv::waitKey(1);
+		if (key == 'p' || key == 'P'){
+			pause = !pause;
+		}
+
+		if (key  == '-'){
+			repause = true;
+			pause = false;
+			sequence.set(CV_CAP_PROP_POS_FRAMES, sequence.get(CV_CAP_PROP_POS_FRAMES)-2); //geht 1 bild zurück
+		}
+
+		if (key  == '+'){
+			repause = true;
+			pause = false;
+		}
+
+
 		if (key == 'q' || key == 'Q' || key == 27)
 			break;
 	}
-
-	return 0;
+	
+		return 0;
 }

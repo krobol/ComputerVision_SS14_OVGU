53f818ec130776b94fd5d024d0e811b4d719b40a
turingmachine42 <twunderling91@gmail.com>
2014-05-06 17:41:44 +0200

    Noise Reduction via Openings + Closings in Foreground + Bounding Boxes + Histogram

--- MyNewOpenCVTestProject/MyNewOpenCVTestProject/main.cpp
+++ MyNewOpenCVTestProject/MyNewOpenCVTestProject/main.cpp
@@ -6,26 +6,31 @@
 #include <vector>
 #include <iostream>
 
+using namespace cv;
+
 int main(int argc, char** argv)
 {
 	// Input Stream from Source
-	cv::VideoCapture sequence("D:/Download/training/2/0000000000.png");
+	VideoCapture sequence("C:/training/2/0000000000.png");
 	if (!sequence.isOpened())
 	{
 		std::cerr << "Failed to open Image Sequence!\n" << std::endl;
 		return 1;
 	}
 
-	cv::Mat frame;	// current frame
-	cv::Mat back;	// background image
-	cv::Mat fore;	// foreground mask
-	cv::Mat structure = cv::Mat::ones(5, 5, CV_8UC1); // strukturelement für dilation
+	Mat frame;	// current frame
+	Mat frameEQ;// current frame equalized
+	Mat back;	// background image
+	Mat fore;	// foreground mask
+	Mat mask;	// mask for Noise Cancelation
+	Mat test;	// for testing purposes
+	vector<Mat> channels;
+	
 	bool pause = false;
 	bool repause = false;
-	cv::BackgroundSubtractorMOG2 mog(50, 3, false);
-	std::vector<std::vector<cv::Point> > contours;
-	//std::cout << "O = " << std::endl << " " << structure << std::endl << std::endl;
-	//cv::namedWindow("Image | q or esc to quit", CV_WINDOW_NORMAL);
+
+	BackgroundSubtractorMOG2 mog(0, 3, true);
+	std::vector<std::vector<Point> > contours;
 
 	while (true)
 	{
@@ -38,24 +43,45 @@ int main(int argc, char** argv)
 			break;
 		}
 
-		mog.operator()(frame, fore);
+		cvtColor(frame, frameEQ, CV_BGR2YCrCb);
+		split(frameEQ, channels);
+		equalizeHist(channels[0], channels[0]);
+		merge(channels, frameEQ);
+		cvtColor(frameEQ, frameEQ, CV_YCrCb2BGR);
+
+		mog.operator()(frameEQ, fore);
 		mog.getBackgroundImage(back);
 
-		cv::erode(fore, fore, cv::Mat::ones(2, 2, CV_8UC1), cv::Point(-1, -1), 1);
-		cv::dilate(fore, fore, structure, cv::Point(-1, -1), 1);
+		fore.copyTo(mask);
+		erode(mask, mask, Mat::ones(3, 3, CV_8UC1));
+		dilate(mask, mask, Mat::ones(20, 20, CV_8UC1));
+		threshold(mask, mask, 250, 255, CV_THRESH_BINARY);
+		Mat clean = mask.mul(fore);
 
-	
+		GaussianBlur(clean, clean, Size(15, 15), 0, 0);
+		threshold(clean, clean, 120, 255, CV_THRESH_BINARY);
 
-		cv::findContours(fore, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+		findContours(clean, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 		for(int i = contours.size()-1; i > -1; i--){
 			if(contours[i].size() < 150)
 				contours.erase(contours.begin()+i);
 		}
-		cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0), 1);
+		drawContours(frame, contours, -1, Scalar(200, 255, 0), 1);
+
+		vector<vector<Point> > contours_poly(contours.size());
+		vector<Rect> boundRect(contours.size());
+
+		for (int i = 0; i < contours.size(); i++)
+		{
+			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+			boundRect[i] = boundingRect(Mat(contours_poly[i]));
+			rectangle(frame, boundRect[i].tl(), boundRect[i].br(), 255, 2, 8, 0);
+		}
 
 		
+
 		imshow("input | q or esc to quit", frame);
-		//imshow("background | q or esc to quit", fore);
+		imshow("background | q or esc to quit", fore);
 
 		if (repause){
 			repause = false;
@@ -65,8 +91,8 @@ int main(int argc, char** argv)
 		}
 
 
-		char key = (char)cv::waitKey(1);
-		if (key == 'p' || key == 'P'){
+		char key = (char)waitKey(1);
+		if (key == ' ' || key == 'p' || key == 'P'){
 			pause = !pause;
 		}
 
